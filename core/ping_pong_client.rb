56de43cb725edda12441af03d4d4d272a9ae421a
Zhussupov Zhassulan <zhzhussupovkz@gmail.com>
2014-04-24 02:12:48 +0600

    add exception handling for ping pong client class

--- core/ping_pong_client.rb
+++ core/ping_pong_client.rb
@@ -17,34 +17,52 @@ class PingPongClient
 
   #set player's coordinate
   def set position, value
-    socket = TCPSocket.new host, port
-    socket.write "SET #{position} #{value}"
-    socket.close
+    begin
+      socket = TCPSocket.new host, port
+      socket.write "SET #{position} #{value}"
+      socket.close_write
+      socket.close
+    rescue Errno::ECONNRESET => e
+      socket.close
+    end
   end
 
   #get player's coordinate
   def get position
-    socket = TCPSocket.new host, port
-    socket.write "GET #{position}"
-    socket.close_write
-    return socket.read
-    socket.close
+    begin
+      socket = TCPSocket.new host, port
+      socket.write "GET #{position}"
+      socket.close_write
+      return socket.read
+      socket.close
+    rescue Errno::ECONNRESET => e
+      socket.close
+    end
   end
 
   #reboot ball coordinates
   def reboot_ball
-    socket = TCPSocket.new host, port
-    socket.write "REBOOT"
-    socket.close_write
-    return socket.read
-    socket.close
+    begin
+      socket = TCPSocket.new host, port
+      socket.write "REBOOT"
+      socket.close_write
+      return socket.read
+      socket.close
+    rescue Errno::ECONNRESET => e
+      socket.close
+    end
   end
 
   #close connection with server
   def close
-    socket = TCPSocket.new host, port
-    socket.write "CLOSE"
-    socket.close
+    begin
+      socket = TCPSocket.new host, port
+      socket.write "CLOSE"
+      socket.close_write
+      socket.close
+    rescue Errno::ECONNRESET => e
+      socket.close
+    end
   end
 
 end
